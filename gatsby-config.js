3d196eb765bae8af8cb1196cba56571d2c58901a
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2020-11-02 16:19:55 +0530

    Modal Routing added in config js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,18 @@ module.exports = {
 		'gatsby-plugin-sass',
 		'gatsby-plugin-smoothscroll',
 		{
+			resolve: `gatsby-plugin-modal-routing`,
+			options: {
+				// A selector to set react-modal's app root to, default is `#___gatsby`
+				// See http://reactcommunity.org/react-modal/accessibility/#app-element
+				appElement: '#___gatsby',
+
+				// Object of props that will be passed to the react-modal container
+				// See http://reactcommunity.org/react-modal/#usage
+				modalProps: {}
+			}
+		},
+		{
 			// `gatsby-plugin-scroll-reveal`,
 			resolve: 'gatsby-plugin-scroll-reveal',
 			options: {
